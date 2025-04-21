26ebe6ead695a76c383c733ce2ae3dcb5df16793
Sasha Jovanovic-Hacon <sashajh2@C02FK47CMD6M.tld>
2025-04-21 11:52:17 -0400

    more root file structure and comments

---
+++ retriever/build_index.py
@@ -0,0 +1,4 @@
+# This doc: 
+# 1. Load documents
+# 2: Embed them
+# 3: Store vectors and metadata in FAISS index
---
+++ scripts/run_rag.py
@@ -0,0 +1,3 @@
+# Load FAISS Index
+# Retrieve top k docs for question
+# Format prompt as Question: ...., Context: { Doc1 : "text", Doc2: "text"}, etc
