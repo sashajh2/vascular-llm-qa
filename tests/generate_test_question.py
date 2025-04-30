0f21dd6662e85d4393930813f0f6df553366541f
Sasha Jovanovic-Hacon <sashajh2@C02FK47CMD6M.tld>
2025-04-30 11:02:29 -0400

    adding and moving test prompts

--- tests/generate_test_question.py
+++ tests/generate_test_question.py
@@ -8,7 +8,7 @@ with open("data/processed/mcq_test.jsonl") as f:
     test_data = [json.loads(line) for line in f]
 
 # === 2. Pick one example ===
-example = test_data[0]  # or pick by index, etc.
+example = test_data[47]  # or pick by index, etc.
 
 # === 3. Retrieve relevant context chunks ===
 relevant_context_chunks = retrieve_docs(example["question"], include_relevant=True, top_k=4)
@@ -18,13 +18,13 @@ relevant_prompt, _ = format_mcq_example(example, relevant_context_chunks, includ
 irrelevant_prompt, _ = format_mcq_example(example, irrelevant_context_chunks, include_relevant=True)
 
 # === 5. Save to .txt file ===
-relevant_output_path = "relevant_formatted_prompt.txt"
+relevant_output_path = "relevant_formatted_prompt_47.txt"
 with open(relevant_output_path, "w") as f:
     f.write(relevant_prompt)
 
 print(f"Relevant Prompt saved to: {relevant_output_path}")
 
-irrelevant_output_path = "irrelevant_formatted_prompt.txt"
+irrelevant_output_path = "irrelevant_formatted_prompt_47.txt"
 with open(irrelevant_output_path, "w") as f:
     f.write(irrelevant_prompt)
 
