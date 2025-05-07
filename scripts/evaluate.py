ef86f98574842168926f7afc85b2bb5a1e492113
Sasha Jovanovic-Hacon <sashajh2@C02FK47CMD6M.tld>
2025-05-07 13:28:26 -0400

    removing unused scripts

--- scripts/evaluate.py
+++
@@ -1,6 +0,0 @@
-# Evaluation Suite
-
-# MCQ Accuracy Check
-# Format Compliance checks
-# Open QA Validity/Similarity check
-# Source/Retrieval Overlap
--- scripts/run_rag.py
+++
@@ -1,4 +0,0 @@
-# Load FAISS Index
-# Retrieve top k docs for question
-# Format prompt as Question: ...., Context: { Doc1 : "text", Doc2: "text"}, etc
-# For this^ use utils.formatting
